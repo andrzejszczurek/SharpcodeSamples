44faeb781c602a342f384decbd4423468eeab3b4
Andrzej Wójcik <andrzej.szczurek91@gmail.com>
2024-07-18 23:32:25 +0200

    menu positions

--- Sharpcode.WebMvc/Helpers/TopMenuProvider.cs
+++ Sharpcode.WebMvc/Helpers/TopMenuProvider.cs
@@ -11,6 +11,8 @@ public class TopMenuProvider
       new MenuItemViewModel() { Id = 1, Name = "Home", Hint = "Home_Hint", Controller = "Home", Action = "Index" },
       new MenuItemViewModel() { Id = 2, Name = "Data transfer", Hint = "", Controller = "DataTransferSample", Action = "Index" },
       new MenuItemViewModel() { Id = 3, Name = "Controls", Hint = "", Controller = "ControlsSample", Action = "Index" },
+      new MenuItemViewModel() { Id = 4, Name = "Form", Hint = "", Controller = "ControlsSample", Action = "Controls" },
+      new MenuItemViewModel() { Id = 5, Name = "Toasts", Hint = "", Controller = "ControlsSample", Action = "Toasts" },
 
    };
 
